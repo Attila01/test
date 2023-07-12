8a4eb6a1af96c5808c216429ea7b84c153c36f5f
Attila01 <48849467+Attila01@users.noreply.github.com>
2023-07-12 14:59:43 -0400

    Update testscript.sh 712-01

--- testscript.sh
+++ testscript.sh
@@ -2,7 +2,7 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "This was added"
-echo "This was added"
-echo "This was added"
-echo "This was added"
+echo "You must be the change you wish to see in the world - GANDHI"
+echo "Love has been mistaken for possession and attachment. Become loving, so you can love all."
+echo "Embrace obstacles to build the resilience required to live a purpose driven life."
+echo "Reaction is ego, so I try not to do it. Responding mindfully is the only way to communicate"
