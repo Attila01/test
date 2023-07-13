5c00e60cfcf34df51daf77cbcbaadec3cca86133
Attila01 <48849467+Attila01@users.noreply.github.com>
2023-07-12 21:04:29 -0400

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -7,3 +7,4 @@ echo "Love has been mistaken for possession and attachment. Become loving, so yo
 echo "Embrace obstacles to build the resilience required to live a purpose driven life."
 echo "Reaction is ego, so I try not to do it. Responding mindfully is the only way to communicate"
 echo "My wealth is not measured in how much time I have to do what I want, rather, how much meaning I’m able to derive from the time I have."
+echo "Do not judge me by my success, judge me by how many times I fell down and got back up again. —Nelson Mandela”
