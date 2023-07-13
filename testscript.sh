dc5ca5cea2bfe10f933c742cb227b82948447498
Attila01 <48849467+Attila01@users.noreply.github.com>
2023-07-12 22:58:54 -0400

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -2,12 +2,4 @@
 echo "this script is from Tshibos"
 NUMBER=$(($NUMBERONE - $NUMBERTWO))
 echo "$NUMBER"
-echo "You must be the change you wish to see in the world - GANDHI"
-echo "Love has been mistaken for possession and attachment. Become loving, so you can love all."
-echo "Embrace obstacles to build the resilience required to live a purpose driven life."
-echo "Reaction is ego, so I try not to do it. Responding mindfully is the only way to communicate"
-echo "My wealth is not measured in how much time I have to do what I want, rather, how much meaning I’m able to derive from the time I have."
-echo "Do not judge me by my success, judge me by how many times I fell down and got back up again. —Nelson Mandela”
-echo “A brave man acknowledges the strength of others. —Veronica Roth.”
-echo “We will either find a way, or make one. ”
-echo “ ”
+
