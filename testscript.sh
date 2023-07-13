2d3c9077017a6b73fb67aa213c1c7b0eae4dac37
Attila01 <48849467+Attila01@users.noreply.github.com>
2023-07-12 22:21:25 -0400

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -9,3 +9,5 @@ echo "Reaction is ego, so I try not to do it. Responding mindfully is the only w
 echo "My wealth is not measured in how much time I have to do what I want, rather, how much meaning I’m able to derive from the time I have."
 echo "Do not judge me by my success, judge me by how many times I fell down and got back up again. —Nelson Mandela”
 echo “A brave man acknowledges the strength of others. —Veronica Roth.”
+echo “We will either find a way, or make one. ”
+echo “ ”
