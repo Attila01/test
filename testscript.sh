321a300975579b76501741299f39023b8c54c093
Attila01 <48849467+Attila01@users.noreply.github.com>
2023-07-17 18:41:01 -0400

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "this script is from Tshibos"
-NUMBER=$(($NUMBERONE - $NUMBERTWO))
-echo "$NUMBER"
+NUMBER1=$(($NUMBERONE - $NUMBERTWO))
+echo "$NUMBER1"
+NUMBER2=$(($NUMBERONE + $NUMBERTWO))
+echo "$NUMBER2"
 
